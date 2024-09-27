cb0b365867d7c92c5731e8aeed80633ed960b816
Silvan Feitosa <silvan.matsuo@hotmail.com>
2024-09-26 22:01:46 -0300

    ToDoList MVVM

--- app/src/main/java/br/edu/ifsp/scl/sdm/todolist/controller/TaskViewModel.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/todolist/controller/TaskViewModel.kt
@@ -1,5 +1,10 @@
 package br.edu.ifsp.scl.sdm.todolist.controller
 
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.room.Room
 import br.edu.ifsp.scl.sdm.todolist.model.database.ToDoListDatabase
 import br.edu.ifsp.scl.sdm.todolist.model.database.ToDoListDatabase.Companion.TO_DO_LIST_DATABASE
@@ -9,12 +14,15 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MainController(private val mainFragment: MainFragment) {
+@Suppress("UNCHECKED_CAST")
+class TaskViewModel (application: Application): ViewModel() {
     private val taskDaoImpl = Room.databaseBuilder(
-        mainFragment.requireContext(),
+        application.applicationContext,
         ToDoListDatabase::class.java,
         TO_DO_LIST_DATABASE
     ).build().getTaskDao()
+
+    val tasksMld = MutableLiveData<List<Task>>()
 
     fun insertTask(task: Task) {
         CoroutineScope(Dispatchers.IO).launch {
@@ -25,7 +33,8 @@ class MainController(private val mainFragment: MainFragment) {
     fun getTasks() {
         CoroutineScope(Dispatchers.IO). launch {
             val tasks = taskDaoImpl.retrieveTasks()
-//            mainFragment.updateTaskList(tasks)
+            tasksMld.postValue(tasks)
+
         }
     }
 
@@ -38,6 +47,14 @@ class MainController(private val mainFragment: MainFragment) {
     fun removeTask(task: Task) {
         CoroutineScope(Dispatchers.IO).launch {
             taskDaoImpl.deleteTask(task)
+        }
+    }
+
+    companion object {
+        val TaskViewModelFactory = object: ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T =
+                TaskViewModel(checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])) as T
+
         }
     }
 }
